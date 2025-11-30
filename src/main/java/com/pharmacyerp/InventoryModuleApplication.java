efd0d512b911f5a08809ddca46383bda69a14cb6
MohamedToukhy <mohamedtd03@gmail.com>
2025-11-30 20:59:18 +0200

    Initial commit

---
+++ src/main/java/com/pharmacyerp/InventoryModuleApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacyerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmacyerp/InventoryModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacyerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
